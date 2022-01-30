7d704bcee23e54f14a30fd28984bb578d5646da8
cryptomuskt <94606333+cryptomuskt@users.noreply.github.com>
2022-01-30 22:59:27 +0900

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,9 @@ const IndexPage = ({
       <Helmet>
         <title>{site.siteMetadata.title}</title>
         <meta name="description" content={site.siteMetadata.description} />
-          <img src="https://raw.githubusercontent.com/cryptomuskt/blog_v5/master/src/images/tech.png" alt="Follow @w3layouts" />
+  <img src="https://raw.githubusercontent.com/cryptomuskt/blog_v5/master/src/images/tech.png" alt="Follow @w3layouts" />
+         <img src="https://engineering.mercari.com/img/ogp/ogp_a.jpg" alt="Follow @w3layouts" />
+           
       </Helmet>
       <HeroHeader/>
       <h2>Blog Posts &darr;</h2>
